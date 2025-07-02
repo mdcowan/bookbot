433e98e0d7c0566f5bbcd0734e480f975a781c80
mdcowan <41978259+mdcowan@users.noreply.github.com>
2025-07-01 21:58:23 -0400

    Added comments for documentation

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from stats import get_num_words
 from stats import get_num_characters
 from stats import get_sorted_num_list
 import sys
+    # The built in sys module provides access to command line arguments. 
+    # In particular sys.argv is a list of strings representing the arguments passed to the script. 
+    # The first argument is the script name, the rest are the arguments.
+    # For example, python3 main.py will result in a sys.argv list that looks like this:
+    # ['main.py']
+    # And python3 main.py books/frankenstein.txt will result in a sys.argv list that looks like this:
+    # ['main.py', 'books/frankenstein.txt']
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -9,12 +16,14 @@ def get_book_text(path_to_file):
     
 def main():
     print("============ BOOKBOT ============")
-    # text_dir = "books/frankenstein.txt"
 
+    # if the length of the sys.argv list doesn't include two arguments (script name + file location)
+    # then tell the user what the correct usage is and exit the program
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-        
+    
+    # pull the second sys.argv argument that should contain the file directory    
     text_dir = sys.argv[1]
     book_text = get_book_text(text_dir)
 
